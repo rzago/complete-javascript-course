835aacd7dc234952e56182af91112fa55f751dd2
rzago <zago.rdg@gmail.com>
2022-04-15 09:46:21 -0300

    lesson 14

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = 'amazing';
 if (js === 'amazing') alert('JavaScript is FUN!');
 
@@ -5,3 +6,46 @@ if (js === 'amazing') alert('JavaScript is FUN!');
 //console.log(40+8+23-10);
 
 // comment
+*/
+
+// true;
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 'Jonas');
+
+// javascriptIsFun = 'YES!';
+// console.log(javascriptIsFun);
+
+// console.log(typeof null);
+
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1991;
+// birthYear = 1990;
+
+// const job;
+
+// var job = 'programmer';
+// job = 'teacher'
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+const firstName = 'Rodrigo';
+const lastName = 'Zago';
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5;
+x += 10;
+x *= 4; 
+x++; // +1
+x--; // -1
+console.log(x);
+
+console.log(ageJonas > ageSarah);
